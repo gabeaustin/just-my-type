124d2631da15e5e17d839af2b1a87e0f738c8ed8
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-04 16:51:05 -0600

    added code that will hide the lc keyboard on shift keydown and show lc keyboard on shift keyup

--- scripts.js
+++ scripts.js
@@ -2,4 +2,20 @@ $(document).ready(function(){
     $("#keyboard-upper-container").hide();
 });
 
+$(document).keydown(function (e) {
+    if (e.keyCode == 16) {
+        // console.log("The shift key was pressed");
+        $("#keyboard-lower-container").hide();
+        $("#keyboard-upper-container").show();
+    }
+
+});
+
+$(document).keyup(function (e) {
+    if (e.keyCode == 16) {
+        // console.log("The shift key was released.");
+        $("#keyboard-lower-container").show();
+        $("#keyboard-upper-container").hide();
+    }
+});
 
