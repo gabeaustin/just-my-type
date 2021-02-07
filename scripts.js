09af89820b57a62c827fe416cf50c04ac832a0c8
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-07 11:19:42 -0600

    WIP

--- scripts.js
+++ scripts.js
@@ -1,41 +1,84 @@
 $(document).ready(function () {
-    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-    let sentenceCounter = 0;
-    let letterCounter = 0;
+    let sentencesArray = [
+        'ten ate neite ate nee enet ite ate inet ent eate', 
+        'Too ato too nOt enot one totA not anot tOO aNot', 
+        'oat itain oat tain nate eate tea anne inant nean', 
+        'itant eate anot eat nato inate eat anot tain eat', 
+        'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
-    let currentSentence = sentences[sentenceCounter]
-    let currentLetter = currentSentence[letterCounter]
-
-    $("#keyboard-upper-container").hide();
-
+    // console.log(sentencesArray[1]);
     //create global sentenceCounter that starts at 0
     //same for letterCounter
+    let sentenceCounter = 0;
+    let letterCounter = 0;
+    let currentSentence = sentencesArray[sentenceCounter];
+    let currentLetter = currentSentence[letterCounter];
+    
+    console.log("before keydown: " + currentLetter );
 
-    $(document).keydown(function (e) {
-        if (e.keyCode == 16) {
-            $("#keyboard-lower-container").hide();
-            $("#keyboard-upper-container").show();
+    $("#keyboard-upper-container").hide();    
+    
+    
+    if(currentSentence.charCodeAt(99) === e.keyCode) {
+        console.log("working?");
+        
+        letterCounter++;
+        currentLetter; /* ??? */
+  /*      
+    }
+        $("#target-letter").empty();
+        $("#target-letter").append(currentLetter);
+        $("#feedback").append("hi");
+        
+        if (currentLetter === " ") {
+            $("#target-letter").append("Space");
+            $("#feedback").append("hi2");
+        } else {
+            $("#target-letter").append(currentSentence);
+        }
+        
+    }
+    
+    console.log(letterCounter);
+    
+    console.log("after keydown: " + currentLetter);
+    
+    //check if e.keyCode matches the current letter in the current sentence
+    //(array indexes)
+    //increment letterCounter up every time this is the case
+// })
+/*
+$(document).keydown(function(e) {
+    if (e.keyCode == 16) {
+        $("#keyboard-lower-container").hide();
+        $("#keyboard-upper-container").show();
+    }
+*/
+
+/*
+$(document).keyup(function (e) {
+    if (e.keyCode == 16) {
+        $("#keyboard-lower-container").show();
+        $("#keyboard-upper-container").hide();
         }
 
-        //check if e.keyCode matches the current letter in the current sentence
-        //(array indexes)
-            //increment letterCounter up every time this is the case
-    })
+        let asciiLetter = e.key.charCodeAt(0); // this catches both lower & uppercase letters
+        console.log("This is Ascii letter after key up: " + asciiLetter);
+        $(`#${asciiLetter}`).css("background-color", "#f5f5f5");
 
-    $(document).keyup(function (e) {
-        let asciiLetter = e.key.charCodeAt(0)
 
-        $(`#${asciiLetter}`).css("background-color", "lightgray")
-        if (e.keyCode == 16) {
-            $("#keyboard-lower-container").show();
-            $("#keyboard-upper-container").hide();
-        }
+        // console.log(("#keyboard-lower-container").value());
     });
-
-
-    $(document).keypress(function (e) {
-        $(`#${e.keyCode}`).css("background-color", "yellow")
+*/
+/*   
+    $(document).keypress(function(e) {
+        $(`#${e.keyCode}`).css("background-color", "yellow");
     });
+*/
+
+    // $(document).show(function(e) {
+    //     $("#sentencesArray");
+    // });
 });
 
 
